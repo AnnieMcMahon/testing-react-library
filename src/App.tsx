81251faf3a7639a0d1670df80139078167c2a2a6
anniemcmahon <anniemcmahon20@gmail.com>
2024-02-24 12:48:21 -0500

    Added Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import { Button } from "react-ts-library";
+import { Button, Card } from "react-ts-library";
+
+const myCard = {
+  id: 4,
+  title: "Write a Bad Song",
+  image: "https://www.leedyer.com/assets/wabsScreenShot.png",
+  technology: "HTML, CSS, JavaScript",
+  summary: "This is A social Media website based around songwriting that encourages quantity over quality, and promotes collaboration. This project was developed by Lee Robert Dyer.",
+  gitHubLink: "https://github.com/leerobertdyer/wabs",
+  gitHubTitle: "leerobertdyer/wabs",
+  webPageLink: "https://writeabadsong.com/",
+  webPageTitle: "writeabadsong.com"
+}
 
 function App() {
-  return <Button label="Easy Button" color="pink"/>;
+  return (
+  <>
+    <Button label="Easy Button" color="pink"/>
+    <Card card={myCard}/>
+  </>
+  )
 }
 
 export default App;
